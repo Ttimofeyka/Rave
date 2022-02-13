d6401244b4adff63ec4ede205c0059d857f35b71
somerandomdev49 <mike.redko49@gmail.com>
2022-02-13 22:16:25 +0300

    Parser and AST stuff

---
+++ source/ast.d
@@ -0,0 +1,77 @@
+module ast;
+
+/// We have two separate syntax trees: for types and for values.
+
+// Abstract type syntax tree node.
+class AtstNode { }
+
+class AtstNodeVoid {}
+
+class AtstNodeName { string name; }
+
+struct FuncSignature {
+	AtstNode ret;
+	AtstNode[] args;
+}
+
+// Abstract syntax tree Node.
+class AstNode { }
+
+class AstNodeFunction : AstNode {
+	string name;
+	AtstNode retType;
+}
+
+enum BinaryOpType {
+	binary_op_add, // addition
+	binary_op_sub, // substraction
+	binary_op_mul, // multiplication
+	binary_op_div, // division
+	binary_op_grt, // greater than
+	binary_op_lst, // less than
+	binary_op_geq, // greater than or equal
+	binary_op_leq, // less than or equal
+	binary_op_eql, // equal to
+	binary_op_neq, // not equal to
+	binary_op_neq, // not equal to
+	binary_op_neq, // not equal to
+	binary_op_neq, // not equal to
+}
+
+class AstNodeBinary : AstNode {
+	AstNode lhs, rhs;
+	BinaryOpType type;
+}
+
+struct Declaration {
+	AtstNode type;
+	string name;
+}
+
+class AstNodeStruct : AstNode {
+	string name; 
+	Declaration[] decls;
+}
+
+class AstNodeIf : AstNode {
+	AstNode cond;
+	AstNode body_;
+	AstNode else_;
+}
+
+class AstNodeWhile : AstNode {
+	AstNode cond;
+	AstNode body_;
+}
+
+class AstNodeAsm : AstNode {
+	string value; // TODO
+}
+
+class AstNodeIden : AstNode { string name; }
+class AstNodeLabel : AstNode { string name; }
+class AstNodeBreak : AstNode { string label; /* optional */ }
+class AstNodeContinue : AstNode { string label; /* optional */ }
+class AstNodeExtern : AstNode { FuncSignature sig; string name; }
+class AstNodeFuncCall : AstNode { AstNode func; AstNode[] args; }
+class AstNodeInt : AstNode { uint value; }
---
+++ source/parser.d
@@ -0,0 +1,11 @@
+module parser;
+
+import std.string;
+import std.array;
+import std.container;
+import std.stdio;
+import ast;
+
+class Parser {
+	
+}
