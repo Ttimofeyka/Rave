2baa164a364b2e28769214d9efbfcb39e650c036
Makcimka132 <pactiks@gmail.com>
2022-02-20 20:51:48 +0300

    gen

--- source/app.d
+++ source/app.d
@@ -33,41 +33,5 @@ void main(string[] args)
 	}
 
 	auto ctx = new GenerationContext();
-
-	LLVMTypeRef* param_types;
-
-	LLVMTypeRef ret_type = LLVMFunctionType(
-		LLVMInt32Type(),
-		param_types,
-		0,
-		false
-	);
-
-	LLVMValueRef mainf = LLVMAddFunction(
-		ctx.mod,
-		cast(const char*)"main",
-		ret_type
-	);
-
-	LLVMBasicBlockRef entry = LLVMAppendBasicBlock(mainf, "entry");
-	LLVMPositionBuilderAtEnd(ctx.builder, entry);
-
-	LLVMValueRef retval = LLVMConstInt(
-		LLVMInt32Type(),
-		cast(ulong)0,
-		false
-	);
-	LLVMBuildRet(ctx.builder, retval);
-
-	char *error;
-	LLVMInitializeNativeTarget();
-	LLVMInitializeNativeAsmPrinter();
-	LLVMInitializeNativeAsmParser();
-
-	LLVMCreateExecutionEngineForModule(&ctx.engine, ctx.mod, &error);
-
-	LLVMGenericValueRef* argss;
-	LLVMGenericValueRef res = LLVMRunFunction(ctx.engine,mainf,0,argss);
-	
-	LLVMWriteBitcodeToFile(ctx.mod, "llvm.bc");
+	ctx.gen();
 }
--- source/gen.d
+++ source/gen.d
@@ -13,4 +13,43 @@ class GenerationContext {
         mod = LLVMModuleCreateWithName(cast(const char*)"epl");
         builder = LLVMCreateBuilder();
     }
+
+    void gen() {
+        LLVMTypeRef* param_types;
+
+	    LLVMTypeRef ret_type = LLVMFunctionType(
+		    LLVMInt32Type(),
+		    param_types,
+		    0,
+		    false
+	    );
+
+	    LLVMValueRef mainf = LLVMAddFunction(
+		    ctx.mod,
+		    cast(const char*)"main",
+		    ret_type
+	    );
+
+	    LLVMBasicBlockRef entry = LLVMAppendBasicBlock(mainf, "entry");
+	    LLVMPositionBuilderAtEnd(ctx.builder, entry);
+
+	    LLVMValueRef retval = LLVMConstInt(
+	    	LLVMInt32Type(),
+	    	cast(ulong)0,
+	    	false
+	    );
+	    LLVMBuildRet(ctx.builder, retval);
+
+	    char *error;
+    	LLVMInitializeNativeTarget();
+    	LLVMInitializeNativeAsmPrinter();
+    	LLVMInitializeNativeAsmParser();
+
+	    LLVMCreateExecutionEngineForModule(&ctx.engine, ctx.mod, &error);
+
+	    LLVMGenericValueRef* argss;
+	    LLVMGenericValueRef res = LLVMRunFunction(ctx.engine,mainf,0,argss);
+	
+	    LLVMWriteBitcodeToFile(ctx.mod, "llvm.bc");
+    }
 }
