7e87fa0f8a4fa6a636189a81e5fd1e471e3d8cb3
Makcimka132 <pactiks@gmail.com>
2022-02-25 09:19:24 +0300

    update x86_64-linux rt

--- rt/x86_64-linux/rt.s
+++ rt/x86_64-linux/rt.s
@@ -5,22 +5,18 @@ exit:
  	movq $60, %rax
   	syscall
 
-.global putch
-putch:
-	push %rdi
+.global write
+write:
 	movq     $1, %rax    # sys_write call number 
 	movq     $1, %rdi    # write to stdout (fd=1)
 	movq     %rsp, %rsi  # use char on stack
 	movq     $1, %rdx    # write 1 char
 	syscall   
-	pop %rdi
 	ret
 
 .global _start
 _start:
 	call main
-	movq $0x21,%rdi
-	call putch
 	movq %rax, %rdi
 	movq $60, %rax
 	syscall
