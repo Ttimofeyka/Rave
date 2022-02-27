7ce363303a689036faab11f32d195f85886ac8fa
Makcimka132 <pactiks@gmail.com>
2022-02-27 17:08:33 +0300

    update rt-s

--- rt/i686-mac/rt.s
+++ rt/i686-mac/rt.s
@@ -5,6 +5,16 @@ exit:
     movl $1, %eax
     int $0x80
 
+.global write
+write:
+	movl $4, %eax
+	movl $1, %ebx
+	movl %ebx, %ecx
+	movl $0, %edx
+
+	int $0x80
+	ret
+
 .global _start
 _start:
     call main
