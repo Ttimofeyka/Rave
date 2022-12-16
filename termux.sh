80c626f4c72be4af33194b78eacb8d8be8838458
Ttimofeyka <pactiks@gmail.com>
2022-12-16 22:14:04 +0300

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -1,6 +1,7 @@
 pkg i wget 
 cpu = uname -m
 wget https://github.com/amuramatsu/termux-packages/releases/download/libllvm11_11.1.0-4/libllvm11_11.1.0-4_aarch64.deb
+dpkg -i libllvm11_11.1.0-4_aarch64.deb
 wget https://github.com/ldc-developers/ldc/releases/download/v1.30.0/ldc2-1.30.0-linux-aarch64.tar.xz
 mkdir _ldc
 tar -xf ldc2-1.30.0-linux-aarch64.tar.xz -C _ldc/
