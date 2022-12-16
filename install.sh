50dd504add36b6408eb9560b7820c6f3a3b210f8
Ttimofeyka <pactiks@gmail.com>
2022-12-16 22:10:16 +0300

    Remove Termux from install.sh

--- install.sh
+++ install.sh
@@ -7,16 +7,11 @@
 
 
 ARCH=0
-TERMUX=0
 
 ISTERMUX="${TERMUX_VERSION:-no}"
 if [ -f "/etc/arch-release" ]; then
     ARCH=1
     echo \[ Rave Installer \] Arch Linux detected, switching to ARCH mode.
-fi
-if [ ISTERMUX -ne "no" ]; then
-    TERMUX=1
-    echo \[ Rave Installer \] Termux detected, switching to TERMUX mode.
 fi
 
 failure ()
@@ -41,30 +36,10 @@ i_llvm11 ()
             failure LLVM-11
         fi
     else
-        if [ "$TERMUX" -eq 1 ]; then
-            pkg i wget
-            cpu = uname -m
-            if [ cpu -eq "aarch64" ]; then
-            wget https://github.com/amuramatsu/termux-packages/releases/download/libllvm11_11.1.0-4/libllvm11_11.1.0-4_aarch64.deb
-                if dpkg -i libllvm11_11.1.0-4/libllvm11_11.1.0-4_aarch64.deb ; then
-                    success LLVM-11
-                else
-                    failure LLVM-11
-                fi
-                wget https://github.com/ldc-developers/ldc/releases/download/v1.30.0/ldc2-1.30.0-linux-aarch64.tar.xz
-                mkdir _ldc
-                tar -xf ldc2-1.30.0-linux-aarch64.tar.xz -C _ldc/
-                echo export PATH=$PATH:~/_ldc/bin > ldc_init.sh
-                echo Write the \"bash command ldc_init.sh\" for the end of the installation.
-            else
-                failure LLVM-11
-            fi
+        if sudo apt install llvm-11 lldb-11 llvm-11-dev libllvm11 llvm-11-runtime clang-11 ; then
+            success LLVM-11
         else
-            if sudo apt install llvm-11 lldb-11 llvm-11-dev libllvm11 llvm-11-runtime clang-11 ; then
-                success LLVM-11
-            else
-                failure LLVM-11
-            fi
+            failure LLVM-11
         fi
     fi
 }
