9a2019bf680adec709ef1d6fc087c320f9434cba
Ttimofeyka <pactiks@gmail.com>
2022-12-16 22:54:30 +0300

    Fix linking on Termux

--- termux.sh
+++ termux.sh
@@ -1,9 +1,2 @@
-pkg i wget 
-cpu = uname -m
-wget https://github.com/amuramatsu/termux-packages/releases/download/libllvm11_11.1.0-4/libllvm11_11.1.0-4_aarch64.deb
-dpkg -i libllvm11_11.1.0-4_aarch64.deb
-wget https://github.com/ldc-developers/ldc/releases/download/v1.30.0/ldc2-1.30.0-linux-aarch64.tar.xz
-mkdir _ldc
-tar -xf ldc2-1.30.0-linux-aarch64.tar.xz -C _ldc/
-echo export PATH=$PATH:~/_ldc/bin > ldc_init.sh
-echo Write the command \"bash ldc_init.sh\" for the end of the installation.
+pkg i tur-repo
+pkg i ldc llvm-11
