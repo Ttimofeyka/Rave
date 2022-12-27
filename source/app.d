eee80ce1e14e7b9d0e916e11b27bc7115723815d
Ttimofeyka <pactiks@gmail.com>
2022-12-27 21:18:57 +0300

    Update arguments parsing

--- source/app.d
+++ source/app.d
@@ -69,7 +69,7 @@ CompOpts analyzeArgs(string[] args) {
 			case "-ol": case "--optimization-level":
 			case "-O": // Compatible with the same option from clang
 				opts.optimizeLevel = to!int(args[idx+1]); idx += 1; break;
-			case "-shared":
+			case "-s": case "--shared":
 				opts.linkparams ~= "-shared "; break;
 			default:
 				if(currCommand[0] == '-') {
