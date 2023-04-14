678c7c117b8d51910b087efcbc73848bc88a3a76
Ttimofeyka <pactiks@gmail.com>
2023-04-14 18:31:16 +0300

    Add support of LLVM-14, clang-14 and gcc (with -no-pie)

--- install.sh
+++ install.sh
@@ -1,69 +1,28 @@
 #!/usr/bin/sh
 
 # Rave Dependencies Installer
-# original by Ttimofeyka
-# modified ver by RedLeader167 (JustKenux)
+# original and modified by Ttimofeyka
+# modified version by RedLeader167 (JustKenux)
 # https://github.com/Ttimofeyka/Rave
 
-
-ARCH=0
-
 if [ -f "/etc/arch-release" ]; then
-    ARCH=1
-    echo \[ Rave Installer \] Arch Linux detected, switching to ARCH mode.
+    echo Arch Linux detected.
+    sudo pacman -S llvm14 ldc dub clang10
+    echo You need to change LLVM-11 to LLVM-14 in dub.json and change compiler into options.json to clang-10.
+    echo If clang-10 does not work, you can use other versions of clang or gcc.
+    echo However, in this case, you should change the compiler.json, replacing the old compiler with its own and adding the -no-pie flag in case of possible errors with PIE/PIC.
+    echo Done.
+else
+    if [ -f "/etc/debian_version" ]; then
+        echo Debian-based detected.
+        echo For compatibility, LLVM-11 is being installed.
+        echo If desired, you can later install other versions of LLVM at your discretion.
+        sudo apt install llvm-11 llvm-11-dev clang-11
+        wget https://downloads.dlang.org/releases/2.x/2.103.0/dmd_2.103.0-0_amd64.deb
+        sudo dpkg -i dmd_2.103.0-0_amd64.deb
+        rm dmd_2.103.0-0_amd64.deb
+        echo Done.
+    else
+        echo Unsupported platform detected.
+    fi
 fi
-
-failure ()
-{
-    echo \[ Rave Installer \] Failed to install $1 - possible problems\: missing in repos, no connection, other errors?
-    echo \[ Rave Installer \] Script terminated.
-    exit 1
-}
-
-success ()
-{
-    echo \[ Rave Installer \] Successfully installed $1.
-}
-
-i_llvm11 ()
-{
-    echo \[ Rave Installer \] Installing LLVM-11
-    if [ "$ARCH" -eq 1 ]; then
-        if sudo pacman -S llvm-11-libs clang10 ; then
-            rm options.json
-            echo -e -n "{\n    \"compiler\": \"clang-10\"\n}" > options.json
-            success LLVM-11
-        else
-            failure LLVM-11
-        fi
-    else
-        if sudo apt install llvm-11 lldb-11 llvm-11-dev libllvm11 llvm-11-runtime clang-11 ; then
-            success LLVM-11
-        else
-            failure LLVM-11
-        fi
-    fi
-}
-
-i_dmd ()
-{
-    echo \[ Rave Installer \] Installing DMD
-    if [ "$ARCH" -eq 1 ]; then
-        if pacman -S dmd ; then
-            success DMD
-        else
-            failure DMD
-        fi
-    else
-        if curl -fsS https://dlang.org/install.sh | bash -s dmd ; then
-            success DMD
-        else
-            failure DMD
-        fi
-    fi
-}
-
-i_llvm11
-i_dmd
-
-echo \[ Rave Installer \] Done.
