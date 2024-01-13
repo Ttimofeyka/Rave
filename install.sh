0b79a10c4a3765df874f387f2eadb547f1915667
Ttimofeyka <pactiks@gmail.com>
2024-01-13 17:43:28 +0300

    Use  for all

--- install.sh
+++ install.sh
@@ -2,21 +2,22 @@
 
 # Rave Dependencies Installer
 
+SUDO=sudo
+if [ -z "$(command -v sudo)" ]; then
+    SUDO=doas
+fi
+
 if [ -f "/etc/arch-release" ]; then
     echo Arch Linux detected.
-    sudo pacman -S llvm15 clang15
+    $SUDO pacman -S llvm15 clang15
     echo Done.
 else
     if [ -f "/etc/debian_version" ]; then
         echo Debian-based detected.
-        sudo apt install llvm-15 llvm-15-dev clang-15 gcc-mingw-w64 binutils-mingw-w64
+        $SUDO apt install llvm-15 llvm-15-dev clang-15 gcc-mingw-w64 binutils-mingw-w64
         echo Done.
     else
         if [ ! -z "$(command -v xbps-install)" ]; then
-            SUDO=sudo
-            if [ -z "$(command -v sudo)" ]; then
-                SUDO=doas
-            fi
             echo Void Linux detected.
             $SUDO xbps-install -S clang llvm
             ./llvm-patch.sh
