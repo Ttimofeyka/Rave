270bf6cf04b20f8f6c13d3b9809e618780010906
Ttimofeyka <pactiks@gmail.com>
2024-01-13 15:35:30 +0300

    Fix install.bat

--- install.bat
+++ install.bat
@@ -5,7 +5,7 @@ choco install make mingw wget
 
 wget --tries=inf https://github.com/terralang/llvm-build/releases/download/llvm-16.0.3/clang+llvm-16.0.3-x86_64-windows-msvc17.7z
 
-if not exist "LLVM" mkdir LLVM-15
+if not exist "LLVM" mkdir LLVM
 if not exist "LLVM/bin" xcopy /s clang+llvm-16.0.3-x86_64-windows-msvc17.7z LLVM
 
 if exist "clang+llvm-16.0.3-x86_64-windows-msvc17" rd /s /q "clang+llvm-16.0.3-x86_64-windows-msvc17"
