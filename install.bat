cc80eb74c4d6fc045b6ccec4bcc07596dc9530d4
Ttimofeyka <pactiks@gmail.com>
2024-01-13 15:39:17 +0300

    Fix install.bat and Makefile for Windows

--- install.bat
+++ install.bat
@@ -5,8 +5,8 @@ choco install make mingw wget
 
 wget --tries=inf https://github.com/terralang/llvm-build/releases/download/llvm-16.0.3/clang+llvm-16.0.3-x86_64-windows-msvc17.7z
 
-if not exist "LLVM" mkdir LLVM
-if not exist "LLVM/bin" xcopy /s clang+llvm-16.0.3-x86_64-windows-msvc17.7z LLVM
+if not exist "LLVM-16" mkdir LLVM-16
+if not exist "LLVM-16/bin" xcopy /s clang+llvm-16.0.3-x86_64-windows-msvc17.7z LLVM-16
 
 if exist "clang+llvm-16.0.3-x86_64-windows-msvc17" rd /s /q "clang+llvm-16.0.3-x86_64-windows-msvc17"
 if exist "clang+llvm-16.0.3-x86_64-windows-msvc17.7z" del "clang+llvm-16.0.3-x86_64-windows-msvc17.7z"
