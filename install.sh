c1418da36a1d9fb7ed96197e9c10a8898135fbc8
Ttimofeyka <pactiks@gmail.com>
2024-01-13 17:50:57 +0300

    Add more beautiful code install.sh

--- install.sh
+++ install.sh
@@ -11,19 +11,24 @@ if [ -f "/etc/arch-release" ]; then
     echo Arch Linux detected.
     $SUDO pacman -S llvm15 clang15
     echo Done.
-else
-    if [ -f "/etc/debian_version" ]; then
-        echo Debian-based detected.
-        $SUDO apt install llvm-15 llvm-15-dev clang-15 gcc-mingw-w64 binutils-mingw-w64
-        echo Done.
-    else
-        if [ ! -z "$(command -v xbps-install)" ]; then
-            echo Void Linux detected.
-            $SUDO xbps-install -S clang llvm
-            ./llvm-patch.sh
-            echo Done.
-        else
-            echo Unsupported platform detected.
-        fi
-    fi
+    exit 0
 fi
+
+
+if [ -f "/etc/debian_version" ]; then
+    echo Debian-based detected.
+    $SUDO apt install llvm-15 llvm-15-dev clang-15 gcc-mingw-w64 binutils-mingw-w64
+    echo Done.
+    exit 0
+fi
+
+
+if [ ! -z "$(command -v xbps-install)" ]; then
+    echo Void Linux detected.
+    $SUDO xbps-install -S clang llvm
+    ./llvm-patch.sh
+    echo Done.
+    exit 0
+fi
+
+echo Unsupported platform detected.
