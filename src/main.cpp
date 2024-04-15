51b9d9e8b41febbcb714dd70287aa9ce30fde168
Ttimofeyka <pactiks@gmail.com>
2024-04-15 19:35:52 +0300

    Add support of -On instead of -O n

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,11 @@ genSettings analyzeArguments(std::vector<std::string>& arguments) {
         else if(arguments[i] == "-ne" || arguments[i] == "--noEntry") settings.noEntry = true;
         else if(arguments[i] == "-ns" || arguments[i] == "--noStd") settings.noStd = true;
         else if(arguments[i] == "-opt" || arguments[i] == "-O" || arguments[i] == "--optimizationLevel") {settings.optLevel = std::stoi(arguments[i+1]); i += 1;}
+        else if(arguments[i] == "-O0") settings.optLevel = 0;
+        else if(arguments[i] == "-O1") settings.optLevel = 1;
+        else if(arguments[i] == "-O2") settings.optLevel = 2;
+        else if(arguments[i] == "-O3") settings.optLevel = 3;
+        else if(arguments[i].find("-O") == 0) settings.optLevel = 1;
         else if(arguments[i] == "-s" || arguments[i] == "--shared") {settings.linkParams += "-shared "; settings.isPIE = true; settings.isPIC = false;}
         else if(arguments[i] == "-sof" || arguments[i] == "--saveObjectFiles") settings.saveObjectFiles = true;
         else if(arguments[i] == "-dw" || arguments[i] == "--disableWarnings") settings.disableWarnings = true;
