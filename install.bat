b9ced038f07591b917ce12775997bef979f98ad9
Ttimofeyka <pactiks@gmail.com>
2024-12-21 13:48:29 +0300

    Update install.bat (LLVM 16 -> 17)

--- install.bat
+++ install.bat
@@ -1,16 +1,14 @@
 @echo off
-Powershell.exe -executionpolicy remotesigned -File ./llvm-patch.ps1
-
 choco install make mingw wget 7zip.install
 
-wget --tries=inf https://github.com/terralang/llvm-build/releases/download/llvm-16.0.3/clang+llvm-16.0.3-x86_64-windows-msvc17.7z
+wget --tries=inf https://github.com/terralang/llvm-build/releases/download/llvm-17.0.5/clang+llvm-17.0.5-x86_64-windows-msvc17.7z
 
-7z x clang+llvm-16.0.3-x86_64-windows-msvc17.7z -oclang+llvm-16.0.3-x86_64-windows-msvc17
+7z x clang+llvm-17.0.5-x86_64-windows-msvc17.7z -oclang+llvm-17.0.5-x86_64-windows-msvc17
 
 if not exist "LLVM" mkdir LLVM
 if not exist "LLVM/bin" xcopy /e /v clang+llvm-16.0.3-x86_64-windows-msvc17 LLVM
 
-if exist "clang+llvm-16.0.3-x86_64-windows-msvc17" rd /s /q "clang+llvm-16.0.3-x86_64-windows-msvc17"
-if exist "clang+llvm-16.0.3-x86_64-windows-msvc17.7z" del "clang+llvm-16.0.3-x86_64-windows-msvc17.7z"
+if exist "clang+llvm-17.0.5-x86_64-windows-msvc17" rd /s /q "clang+llvm-17.0.5-x86_64-windows-msvc17"
+if exist "clang+llvm-17.0.5-x86_64-windows-msvc17.7z" del "clang+llvm-17.0.5-x86_64-windows-msvc17.7z"
 
 pause
