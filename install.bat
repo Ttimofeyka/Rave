58fdbd50ca04b6310db0b8c038f474b3b50badc6
BuildTools <unconfigured@null.spigotmc.org>
2025-03-06 16:15:50 +0300

    Fix install.bat

--- install.bat
+++ install.bat
@@ -6,7 +6,7 @@ wget --tries=inf https://github.com/terralang/llvm-build/releases/download/llvm-
 7z x clang+llvm-17.0.5-x86_64-windows-msvc17.7z -oclang+llvm-17.0.5-x86_64-windows-msvc17
 
 if not exist "LLVM" mkdir LLVM
-if not exist "LLVM/bin" xcopy /e /v clang+llvm-16.0.3-x86_64-windows-msvc17 LLVM
+if not exist "LLVM/bin" xcopy /e /v clang+llvm-17.0.5-x86_64-windows-msvc17 LLVM
 
 if exist "clang+llvm-17.0.5-x86_64-windows-msvc17" rd /s /q "clang+llvm-17.0.5-x86_64-windows-msvc17"
 if exist "clang+llvm-17.0.5-x86_64-windows-msvc17.7z" del "clang+llvm-17.0.5-x86_64-windows-msvc17.7z"
