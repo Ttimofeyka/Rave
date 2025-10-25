815292f439cfb9f943cbfbb8f8027b1e5e0a3d42
Ttimofeyka <pactiks@gmail.com>
2025-10-25 21:40:58 +0300

    NodeIden refactoring

--- src/parser/nodes/NodeIden.cpp
+++ src/parser/nodes/NodeIden.cpp
@@ -5,32 +5,27 @@ with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
 #include "../../include/parser/nodes/NodeIden.hpp"
+#include "../../include/parser/ast.hpp"
 #include "../../include/parser/nodes/NodeFunc.hpp"
 #include "../../include/parser/nodes/NodeVar.hpp"
 #include "../../include/parser/nodes/NodeGet.hpp"
-#include "../../include/utils.hpp"
-#include "../../include/parser/ast.hpp"
 
-NodeIden::NodeIden(std::string name, int loc) {
-    this->name = name;
-    this->loc = loc;
-}
+NodeIden::NodeIden(std::string name, int loc, bool isMustBePtr) 
+    : name(name), loc(loc), isMustBePtr(isMustBePtr) {}
 
-NodeIden::NodeIden(std::string name, int loc, bool isMustBePtr) {
-    this->name = name;
-    this->loc = loc;
-    this->isMustBePtr = isMustBePtr;
-}
+NodeIden::NodeIden(std::string name, int loc) : name(name), loc(loc), isMustBePtr(false) {}
 
 Node* NodeIden::copy() { return new NodeIden(name, loc, isMustBePtr); }
 
 void NodeIden::check() { isChecked = true; }
 
 Type* getIdenType(std::string idenName, int loc) {
-    if (AST::aliasTable.find(idenName) != AST::aliasTable.end()) return AST::aliasTable[idenName]->getType();
-    if (AST::funcTable.find(idenName) != AST::funcTable.end()) return AST::funcTable[idenName]->getType();
+    if (auto it = AST::aliasTable.find(idenName); it != AST::aliasTable.end()) return it->second->getType();
+    if (auto it = AST::funcTable.find(idenName); it != AST::funcTable.end()) return it->second->getType();
+
     if (!currScope->has(idenName) && !currScope->hasAtThis(idenName)) {
-        if (generator->toReplace.find(idenName) != generator->toReplace.end()) return getIdenType(generator->toReplace[idenName]->toString(), loc);
+        if (auto it = generator->toReplace.find(idenName); it != generator->toReplace.end()) 
+            return getIdenType(it->second->toString(), loc);
 
         generator->error("unknown identifier \033[1m" + idenName + "\033[22m!", loc);
         return nullptr;
@@ -39,47 +34,41 @@ Type* getIdenType(std::string idenName, int loc) {
     return currScope->getVar(idenName, loc)->getType();
 }
 
-Type* NodeIden::getType() {
-    return getIdenType(name, loc);
-}
+Type* NodeIden::getType() { return getIdenType(name, loc); }
 
 Node* NodeIden::comptime() {
-    if (generator != nullptr && generator->toReplaceValues.find(name) != generator->toReplaceValues.end()) return generator->toReplaceValues[name]->comptime();
+    if (auto it = generator->toReplaceValues.find(name); it != generator->toReplaceValues.end()) 
+        return it->second->comptime();
 
-    if (AST::aliasTable.find(name) == AST::aliasTable.end()) {
+    auto it = AST::aliasTable.find(name);
+    if (it == AST::aliasTable.end()) {
         generator->error("unknown alias \033[1m" + name + "\033[22m!", loc);
         return nullptr;
     }
 
-    return AST::aliasTable[name];
+    return it->second;
 }
 
 RaveValue NodeIden::generate() {
-    if (AST::aliasTable.find(name) != AST::aliasTable.end()) return AST::aliasTable[name]->generate();
+    if (auto it = AST::aliasTable.find(name); it != AST::aliasTable.end()) return it->second->generate();
 
-    if (generator->globals.find(name) != generator->globals.end()) {
+    if (auto it = generator->globals.find(name); it != generator->globals.end()) {
         AST::varTable[name]->isUsed = true;
-
-        if (isMustBePtr) return generator->globals[name];
-        return LLVM::load(generator->globals[name], "NodeIden_load", loc);
+        return isMustBePtr ? it->second : LLVM::load(it->second, "NodeIden_load", loc);
     }
 
-    if (currScope != nullptr) {
-        if (currScope->has(name) || currScope->hasAtThis(name)) {
-            if (currScope->localVars.find(name) != currScope->localVars.end()) currScope->localVars[name]->isUsed = true;
-
-            if (isMustBePtr) return currScope->getWithoutLoad(name, loc);
-            return currScope->get(name, loc);
-        }
+    if (currScope && (currScope->has(name) || currScope->hasAtThis(name))) {
+        if (auto it = currScope->localVars.find(name); it != currScope->localVars.end()) 
+            it->second->isUsed = true;
+        return isMustBePtr ? currScope->getWithoutLoad(name, loc) : currScope->get(name, loc);
     }
 
-    if (AST::funcTable.find(name) != AST::funcTable.end()) {
-        if (generator->functions.find(name) == generator->functions.end()) AST::funcTable[name]->generate();
-
+    if (auto it = AST::funcTable.find(name); it != AST::funcTable.end()) {
+        if (generator->functions.find(name) == generator->functions.end()) it->second->generate();
         generator->addAttr("noinline", LLVMAttributeFunctionIndex, generator->functions[name].value, loc);
         return generator->functions[name];
     }
-    
+
     generator->error("unknown identifier \033[1m" + name + "\033[22m!", loc);
     return {};
 }
