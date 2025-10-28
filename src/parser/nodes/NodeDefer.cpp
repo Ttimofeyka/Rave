b10114a103523f06fcd630af37bf66001d0b8a88
Ttimofeyka <pactiks@gmail.com>
2025-10-28 16:06:51 +0300

    NodeDefer refactoring

--- src/parser/nodes/NodeDefer.cpp
+++ src/parser/nodes/NodeDefer.cpp
@@ -9,29 +9,15 @@ with this file, You can obtain one at htypep://mozilla.org/MPL/2.0/.
 #include "../../include/parser/nodes/NodeRet.hpp"
 
 void Defer::make(Node* value, bool isFunctionScope) {
-    if (!isFunctionScope) {
-        if (generator->activeLoops.size() > 0) {
-            Loop loop = generator->activeLoops[generator->activeLoops.size() - 1];
-            LLVMBasicBlockRef oldBB = generator->currBB;
+    LLVMBasicBlockRef targetBlock = nullptr;
 
-            LLVM::Builder::atEnd(loop.end);
+    // Determine target block: loop end for non-function scope, or function end
+    if (!isFunctionScope && !generator->activeLoops.empty()) targetBlock = generator->activeLoops.rbegin()->second.end;
+    else if (currScope->fnEnd) targetBlock = currScope->fnEnd;
 
-            auto oldScope = currScope;
-            currScope = copyScope(oldScope);
-
-            value->generate();
-
-            LLVM::Builder::atEnd(oldBB);
-
-            delete currScope;
-            currScope = oldScope;
-            return;
-        }
-    }
-
-    if (currScope->fnEnd != nullptr) {
+    if (targetBlock) {
         LLVMBasicBlockRef oldBB = generator->currBB;
-        LLVM::Builder::atEnd(currScope->fnEnd);
+        LLVM::Builder::atEnd(targetBlock);
 
         auto oldScope = currScope;
         currScope = copyScope(oldScope);
@@ -58,6 +44,6 @@ Node* NodeDefer::comptime() { return this; }
 
 Node* NodeDefer::copy() { return new NodeDefer(loc, instruction->copy(), isFunctionScope); }
 
-NodeDefer::~NodeDefer() { if (instruction != nullptr) delete instruction; }
+NodeDefer::~NodeDefer() { if (instruction) delete instruction; }
 
 RaveValue NodeDefer::generate() { Defer::make(instruction, isFunctionScope); return {}; }
